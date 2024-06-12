6570d987e1409b2bfbc953e5759a771e8f6c910f
luckybutter1011 <luckybutter322982@gmail.com>
2024-06-12 19:49:39 +1000

    update_test

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -7,14 +7,18 @@ const scriptsInEvents = {
 	{
 		// Get UserID
 		const param = new URLSearchParams(window.location.search);
+		console.log("Working")
+		runtime.globalVars.User_ID = 989868;
 		
 		if(param.has('user_id'))
 		{
 			console.log("param exists");
-			paramGotten = param.get('myparam');
+			paramGotten = param.get('user_id');
 			console.log(paramGotten);
 			runtime.globalVars.User_ID = paramGotten;
 		}
+		
+		fetch("")
 	}
 
 };
