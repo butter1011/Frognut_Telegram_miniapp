9fee9dbb8f1ed8130b51d245bfb90cbe5d90f0b8
luckybutter1011 <luckybutter322982@gmail.com>
2024-07-05 04:47:09 +1000

    revert the invite

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -6,11 +6,6 @@ const scriptsInEvents = {
 	async Gamecode_Event70_Act31(runtime, localVars)
 	{
 		runtime.callFunction("SaveData");
-	},
-
-	async Gamecode_Event148_Act2(runtime, localVars)
-	{
-		globalThis.inviteuser(runtime);
 	}
 
 };
